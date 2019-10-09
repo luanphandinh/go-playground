db538c2c9176ba727a11a1598b831aefbfe72df8
Luan Phan <luanphandinh@outlook.com>
2019-10-09 19:58:42 +0700

    tuning IO Bound result

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # go-playground
+
+This projects is for some fun examples.
+
+#`io_bound_tune_test.go`
+The result is very much depend on the server response, but ultimately the result from running concurrent is remarkable faster.
+```bash
+GOGC=off go test -cpu 1 -run none -bench . -benchtime 3s
+goos: darwin
+goarch: amd64
+pkg: github.com/luanphandinh/go-playground
+BenchmarkSequential            1        5396369911 ns/op
+BenchmarkConcurrent            3        1481725928 ns/op : ~72.5% faslter
+PASS
+ok      github.com/luanphandinh/go-playground   14.246s
+```
---
+++ io_bound_tune_test.go
@@ -0,0 +1,54 @@
+// Example of IO-Bound speed up
+// When sending multiple get request to server
+// Could speed up by using go routines.
+package go_playground
+
+import (
+	"net/http"
+	"sync"
+	"testing"
+)
+
+// Simple get request
+func request() *http.Request {
+	url := "https://baconator-bacon-ipsum.p.rapidapi.com/?type=all-meat"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("x-rapidapi-host", "baconator-bacon-ipsum.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "722490e69dmshc56b43fc01c02c8p14c364jsn2e38d21dc2a1")
+	return req
+}
+
+// Sequential fetch
+func fetch() {
+	for i := 0; i < 5; i++ {
+		res, _ := http.DefaultClient.Do(request())
+		defer res.Body.Close()
+	}
+}
+
+// Concurrent fetch
+func fetchConcurrent() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			res, _ := http.DefaultClient.Do(request())
+			defer res.Body.Close()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkSequential(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fetch()
+	}
+}
+
+func BenchmarkConcurrent(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fetchConcurrent()
+	}
+}
