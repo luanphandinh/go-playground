93b1a6cad5f85629dc7091de2eab0fa649635b1c
Luan Phan <luanphandinh@outlook.com>
2022-01-11 21:38:14 +0700

    Revise row, col traverse tune test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luanphandinh/go-tuning-examples
+
+go 1.16
--- row_or_column_traverse_tune_test.go
+++ row_or_column_traverse_tune_test.go
@@ -1,26 +1,46 @@
 package go_tuning_examples
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-const DIMENSION = 10000
-var arr [DIMENSION][DIMENSION]int
+const ROWS = 10000
+const COLS = 10000
 
-func rowTraverse() int {
-	count := 0
-	for i := 0; i < DIMENSION; i++ {
-		for j := 0; j < DIMENSION; j++ {
-			count += arr[i][j]
+var arr [][]int64
+
+func initTraverseTestArray() {
+	fmt.Println("Init array")
+	arr = make([][]int64, ROWS)
+	for i := 0; i < ROWS; i++ {
+		arr[i] = make([]int64, COLS)
+	}
+
+	for i := 0; i < ROWS; i++ {
+		for j := 0; j < COLS; j++ {
+			arr[i][j] = int64(i + j)
+		}
+	}
+	fmt.Println("Init array done.")
+}
+
+func rowTraverse() int64 {
+	count := int64(0)
+	for row := 0; row < ROWS; row++ {
+		for col := 0; col < COLS; col++ {
+			count += arr[row][col]
 		}
 	}
 
 	return count
 }
 
-func colTraverse() int {
-	count := 0
-	for i := 0; i < DIMENSION; i++ {
-		for j := 0; j < DIMENSION; j++ {
-			count += arr[j][i]
+func colTraverse() int64 {
+	count := int64(0)
+	for col := 0; col < COLS; col++ {
+		for row := 0; row < ROWS; row++ {
+			count += arr[row][col]
 		}
 	}
 
@@ -28,6 +48,7 @@ func colTraverse() int {
 }
 
 func BenchmarkRowTraverse(b *testing.B) {
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		rowTraverse()
 	}
